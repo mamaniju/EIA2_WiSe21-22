7512e27ef9ddd0b9e5cf7658eed163eff9247d42
mamaniju <julia.dolores.mamani@hs-furtwangen.de>
2021-10-16 00:23:23 +0200

    test

--- Aufgabe_L01/L01_script.ts
+++ Aufgabe_L01/L01_script.ts
@@ -3,11 +3,9 @@ namespace AufgabeL01 {
     let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zauber", "zerstoert"];
     let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-
     //console.log(subjekte);
     //console.log(praedikate);
     //console.log(objekte);
-
     for (let i: number = subjekte.length; i >= 1; i--) {
         //console.log(i);
         let funktionArray: string = getVerse(subjekte, praedikate, objekte);
@@ -20,14 +18,10 @@ namespace AufgabeL01 {
         let randomPraedikate: number = Math.floor (Math.random() * praedikate.length); 
         vers += praedikate.splice(randomPraedikate, 1) [0] + " ";
         let randomObjekte: number = Math.floor (Math.random() * objekte.length); 
-        vers += objekte.splice(randomObjekte, 1) [0] + " ";
-        
+        vers += objekte.splice(randomObjekte, 1) [0] + " ";     
         //console.log(_subjekte, _praedikate, _objekte);
         //console.log(getVerse);
         //console.log(randomSubjekte, randomPraedikate, randomObjekte);
-
         return (vers);
-
     }
-
 }
